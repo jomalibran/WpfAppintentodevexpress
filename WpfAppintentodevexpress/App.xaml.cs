16a9079c4842f5ac105f3003faf5a5e1fed32940
Orlando Escamilla <joelorlandom@gmail.com>
2024-04-19 16:01:53 -0600

    first commit

---
+++ WpfAppintentodevexpress/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WpfAppintentodevexpress
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow view = new MainWindow();
+            MainWindowVM context = new MainWindowVM();
+            view.DataContext = context;
+            view.Show();
+        }
+    }
+}
---
+++ WpfAppintentodevexpress/MainWindowVM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Mvvm;
+
+namespace WpfAppintentodevexpress
+{
+    public class MainWindowVM : DevExpress.Mvvm.ViewModelBase
+    {
+        public MainWindowVM()
+        {
+            cargarInfo();
+        }
+
+        public void cargarInfo()
+        {
+            using (Data.DataSet1TableAdapters.PaisesTableAdapter ta = new Data.DataSet1TableAdapters.PaisesTableAdapter())
+            {
+                Data.DataSet1.PaisesDataTable dt = new Data.DataSet1.PaisesDataTable();
+                dt = ta.GetData();
+
+                Datasource = dt.ToList();
+            }
+        }
+
+
+        public List<Data.DataSet1.PaisesRow> Datasource
+        {
+            get { return GetProperty(() => Datasource); }
+            set { SetProperty(() => Datasource, value); }
+        }
+    }
+
+    public class Paises
+    {
+        public string CLAVE_PAIS { get; set; }
+        public string NOMBRE_PAIS { get; set; }
+    }
+}
